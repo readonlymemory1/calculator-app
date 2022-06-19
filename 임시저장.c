50b73a6a565b12f5584e273a56b507ba28b37d98
readonlymemory1 <100295423+readonlymemory1@users.noreply.github.com>
2022-06-19 12:11:15 +0900

    Update 임시저장.c

--- 임시저장.c
+++ 임시저장.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+int main(){
+	int a;
+	int b;
+	int c;
+	int d;
+	scanf_s("%d", &a);
+	for (/*c = 1;c <= b;c++*/c = 1; c <= a; c++) {
+		
+		if (a % c == 0) {
+			for (b = 0;b <= a;b++) {
+
+				printf("*");
+			}
+			printf("\n");
+		}
+	}
+			
+		
+	
+
+}
